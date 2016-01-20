398864727fa8dbe5791ca15fed787b4d4d342ac4
TheOtherDave <davesweeris@mac.com>
2016-01-19 17:48:55 -0800

    Initial Commit

---
+++ EnumEnhancer/EnumEnhancer.h
@@ -0,0 +1,19 @@
+//
+//  EnumEnhancer.h
+//  EnumEnhancer
+//
+//  Created by David Sweeris on 2016/01/19.
+//  Copyright © 2016 David Sweeris. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EnumEnhancer.
+FOUNDATION_EXPORT double EnumEnhancerVersionNumber;
+
+//! Project version string for EnumEnhancer.
+FOUNDATION_EXPORT const unsigned char EnumEnhancerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnumEnhancer/PublicHeader.h>
+
+
